9ba4df217ef02842798e5e1044dd052c99f0fd9f
Syed-SearchEndeca <71517603+Syed-SearchEndeca@users.noreply.github.com>
2022-04-16 16:14:56 +0530

    Add files via upload

---
+++ springconfig/src/main/java/com/searchendeca/message/MessageController.java
@@ -0,0 +1,39 @@
+package com.searchendeca.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController {
+	
+	@Autowired
+	private MessageExchangeService proxy;
+
+	@Autowired
+	private GlobalProperties global;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return global.getGreetmsg();
+    }
+    
+    @GetMapping("/employee/message/{id}")
+    public String getEmployeeMessage(@PathVariable String id) {
+        return id+":"+global.getGreetmsg();
+    }
+    
+    @GetMapping("/employee-feign/message/{id}")
+    public String getEmployeeFeignMessage(@PathVariable String id) {
+    	String response = proxy.getEmployeeMessage(id);
+        return response;
+    }
+    
+    
+
+    
+    
+}
